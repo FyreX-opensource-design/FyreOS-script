93b463dc69702ca8a08535ffd9658132264d0838
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2024-08-28 11:12:19 -0500

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -35,27 +35,39 @@ yay -Sy microsoft-edge-stable-bin
 else if [$browser_result == "3"]; then
 sudo pacman -Sy firefox
 fi
-read "install waydroid? this allows for use of most android apps within Linux via a LXC [Y/N]" waydroid_result
-if [$waydroid_result == "Y"]; then
+read "install waydroid? this allows for use of most android apps within Linux via a LXC [y/n]" waydroid_result
+if [$waydroid_result == "y"]; then
 yay -Sy binder_linux-dkms
 sudo modprobe binder-linux device=binder,hwbinder,vndbinder
 yay -Sy waydroid
 fi
 read "install bottles? This will allow you to run most windows apps as if they were designed for linux? do note, it sandboxes them from your system somewhat [Y/N]" bottles_result
-if [$bottles_result == "Y"]; then
-  flatpak install flathub com.usebottles.bottles
+if [$bottles_result == "y"]; then
+  flatpak install flathub com.usebottles.bottles -y
 fi
-read "install steam? [Y/N]" steam_result
+read "install steam? [y/n]" steam_result
 if [$steam_result == "y"]; then
   sudo pacman -Sy steam
-  
+  read "install packages to manage proton? e.g. ProtonPlus, Proton Tricks, ProtonupQT [y/n]" proton_supp_result
+  if [$proton_supp_result == "y"]; then
+    flatpak install flathub com.vysp3r.ProtonPlus net.davidotek.pupgui2 com.github.Matoking.protontricks -y
+    echo "\nuse 'flatpak override --user --filesystem=/path/to/other/Steam/Library com.github.Matoking.protontricks' to allow ProtonUpQT to access other drives" >> ~/tips.txt
+  fi
+  read "install Heroic Launcher? This is for games off of Epic [y/n]" hero_result
+  if [$hero_result == "y"]; then
+    yay -Sy heroic-games-launcher
+  fi
 fi
-read "install packages for 3d printing/general CAD [Y/N]" cad_result
-if [$cad_result == "Y"]; then
+read "install Celeste? This allows you to sync almost any cloud storage to your filesystem [y/n]" cloud_result
+if [$cloud_result == "y"]; then
+  flatpak install flathub com.hunterwittenborn.Celeste -y
+fi
+read "install packages for 3d printing/general CAD [y/n]" cad_result
+if [$cad_result == "y"]; then
   yay -Sy stl-thumb
   yay -Sy stl-thumb-kde
-  read "use cura? [Y/N]" cura_result
-  if [$cura_result == "Y"]; then
+  read "use cura? [y/n]" cura_result
+  if [$cura_result == "y"]; then
     flatpak install flathub com.ultimaker.cura
   fi
 fi
