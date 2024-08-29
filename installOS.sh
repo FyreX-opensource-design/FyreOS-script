ec750a8a28b933842962a542b51f4c5bbc00072d
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2024-08-29 10:44:35 -0500

    Update installOS.sh

--- installOS.sh
+++ installOS.sh
@@ -29,21 +29,35 @@ check https://nowsci.com/winapps/ for running most windows apps in linux, potent
 #user custom stuff
 read "which browser do you want to use most often: 1 = chrome, 2 = microsoft edge, 3 = firefox" browser_result
 if [$browser_result == "1"]; then
-yay -Sy google-chrome
+  yay -Sy google-chrome
 else if [$browser_result == "2"]; then
-yay -Sy microsoft-edge-stable-bin
+  yay -Sy microsoft-edge-stable-bin
 else if [$browser_result == "3"]; then
-sudo pacman -Sy firefox
+  sudo pacman -Sy firefox
 fi
 read "install waydroid? this allows for use of most android apps within Linux via a LXC [y/n]" waydroid_result
 if [$waydroid_result == "y"]; then
-yay -Sy binder_linux-dkms
-sudo modprobe binder-linux device=binder,hwbinder,vndbinder
-yay -Sy waydroid
+  yay -Sy binder_linux-dkms
+  sudo modprobe binder-linux device=binder,hwbinder,vndbinder
+  yay -Sy waydroid
+fi
+read "install distrobox? This will let you run linux packages designed for other distros without needing to go through the AUR [y/n]" distro_result
+if [$distro_result == "y"]; then
+  sudo pacman -Sy distrobox podman
+  read "install Box Buddy? This is a Distrobox manager [y/n]" box_result
+  if [$box_result == "y"]; then
+    flatpak install flathub io.github.dvlv.boxbuddyrs -y
+  fi
 fi
 read "install bottles? This will allow you to run most windows apps as if they were designed for linux? do note, it sandboxes them from your system somewhat [Y/N]" bottles_result
 if [$bottles_result == "y"]; then
   flatpak install flathub com.usebottles.bottles -y
+fi
+read "which office suite do you want to install? 1 = WPS, 2 = Libre, 3 = None" office_result
+if [$office_result == "1"]; then
+  flatpak install flathub com.wps.Office -y
+else if [$office_result == "2"]; then
+  flatpak install flathub org.libreoffice.LibreOffice -y
 fi
 read "install steam? [y/n]" steam_result
 if [$steam_result == "y"]; then
@@ -62,12 +76,16 @@ read "install Celeste? This allows you to sync almost any cloud storage to your 
 if [$cloud_result == "y"]; then
   flatpak install flathub com.hunterwittenborn.Celeste -y
 fi
-read "install packages for 3d printing/general CAD [y/n]" cad_result
+read "install graphics tablet/android as display software? 1 = Android Virtual Pen, 2 = Weylus Comunnity Edition, 3 = both" grapthic_result
+if [$grapthic_result == "1"]; then
+  flatpak install flathub io.github.androidvirtualpen.virtualpen -y
+else if [$grapthic_result == "2"]; then
+  flatpak install flathub io.github.electronstudio.WeylusCommunityEdition -y
+else if [$grapthic_result == "3"]; then
+  flatpak install flathub io.github.electronstudio.WeylusCommunityEdition io.github.androidvirtualpen.virtualpen -y
+fi
+read "install packages for STL thumbnails [y/n]" cad_result
 if [$cad_result == "y"]; then
   yay -Sy stl-thumb
   yay -Sy stl-thumb-kde
-  read "use cura? [y/n]" cura_result
-  if [$cura_result == "y"]; then
-    flatpak install flathub com.ultimaker.cura
-  fi
 fi
